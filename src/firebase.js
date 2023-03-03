7558376ea12220201bd4ed62950297b7dd041d1b
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2023-03-03 06:24:16 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAek7mp4gLDZxmq2cKBQxBK9z9xTdcNmW0",
@@ -15,13 +12,11 @@ const firebaseConfig = {
     measurementId: "G-4YVSVYFSQ3"
   };
 
-  // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth();
+  const firebaseApp =firebase.initializeApp(firebaseConfig);
+  const auth =firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
 
-const provider = new GoogleAuthProvider();
-const db = getFirestore(firebaseApp);
+  const db = firebaseApp.firestore();
 
-
-export { auth, provider };
-export default db;
+  export{auth, provider};
+  export default db;
