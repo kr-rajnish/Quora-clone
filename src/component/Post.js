a0589559697da50b7f1c7fc384605d9e039e6c47
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2023-03-03 07:22:54 +0530

    Add files via upload

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,97 +1,144 @@
-import React, { useEffect, useState } from 'react';
-import { ArrowDownwardOutlined, ArrowUpwardOutlined, ChatBubbleOutlineOutlined, MoreHorizOutlined, RepeatOutlined, ShareOutlined } from '@mui/icons-material';
-import { Avatar } from '@mui/material';
+import {
+  ArrowDownwardOutlined,
+  ArrowUpwardOutlined,
+  ChatBubbleOutlineOutlined,
+  MoreHorizOutlined,
+  RepeatOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import "../css/Post.css";
-import Modal from 'react-modal'
-import { useDispatch, useSelector } from 'react-redux';
-import { selectQuestionId, selectQuestionName, setQuestionInfo } from '../features/questionSlice';
-import firebase from 'firebase/compat/app';
-import db from '../firebase'
-import { selectUser } from '../features/userSlice';
+import Modal from "react-modal";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { selectUser } from "../features/userSlice";
+import { selectQuestionId, setQuestionInfo } from "../features/questionSlice";
 
+Modal.setAppElement("#root");
 
-const Post = ({id, question, imgUrl, timestamp, quoraUser}) => {
-
+function Post({ Id, question, image, timestamp, quoraUser }) {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
-    const [openModal, setOpenModal]= useState(false);
-    const questionId = useSelector(selectQuestionId);
-    const questionName = useSelector(selectQuestionName);
-    const [answer, setAnswer]=useState('');
-    const [getAnswers, setGetAnswers]= useState([]);
-
-
+  const [IsmodalOpen, setIsModalOpen] = useState(false);
+  const questionId = useSelector(selectQuestionId);
+  const [answer, setAnswer] = useState("");
+  const [getAnswers, setGetAnswers] = useState([]);
 
   useEffect(() => {
-    if(questionId){
-      db.collection('questions').doc(questionId).collection('answer')
-      .orderBy('timestamp', 'desc').onSnapshot((snapshot) =>
-      setGetAnswers(
-        snapshot.docs.map((doc) =>({id: doc.id, answers: doc.data()}))
-      ))
-      
+    if (questionId) {
+      const q = query(
+        collection(db, "questions", questionId, "answer"),
+        orderBy("timestamp", "desc")
+      );
+
+      const unsubscribe = onSnapshot(q, (snapshot) =>
+        setGetAnswers(
+          snapshot.docs.map((doc) => ({ id: doc.id, answers: doc.data() }))
+        )
+      );
+
+      return () => {
+        unsubscribe();
+      };
     }
-  }, [questionId])
-  
-  
-  
-  const handleAnswer = (e) => {
+  }, [questionId]);
+
+  const handleAnswer = async (e) => {
     e.preventDefault();
 
-    if (questionId) {  
-      db.collection("questions").doc(questionId).collection("answer").add({
-        user: user,
-        answer: answer,
-        questionId: questionId,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      });
+    if (questionId) {
+      try {
+        const docRef = await addDoc(
+          collection(db, "questions", questionId, "answer"),
+          {
+            user: user,
+            answer: answer,
+            questionId: questionId,
+            timestamp: serverTimestamp(),
+          }
+        );
+        console.log("Answer written with ID: ", docRef.id);
+      } catch (error) {
+        console.error("Error adding answer: ", error);
+      }
     }
-    console.log(questionId, questionName);
     setAnswer("");
-    setOpenModal(false);
+    setIsModalOpen(false);
   };
 
-
   return (
-    <div className='post' 
-    onClick={() => dispatch(setQuestionInfo({
-        questionId:id,
-        questionName:question
-      })
-      )
-      } >
-     <div className="post_info">
-        <Avatar src={user.photo} />
-        <h5> {user.displayName ? user.displayName: user.email}</h5>
+    <div
+      className="post"
+      onClick={() =>
+        dispatch(
+          setQuestionInfo({
+            questionId: Id,
+            questionName: question,
+          })
+        )
+      }
+    >
+      <div className="post__info">
+        <Avatar
+          src={
+            quoraUser.photo
+              ? quoraUser.photo
+              : "https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+          }
+          imgProps={{ referrerPolicy: "no-referrer" }}
+        />
+        <h5>
+          {quoraUser.displayName ? quoraUser.displayName : quoraUser.email}
+        </h5>
         <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
-     </div>
-     <div className="post_body">
-        <div className="post_question">
-            <p>{question}</p>
-            <button className="post_btnAnswer"  onClick={() => setOpenModal(true)}>Answer</button>
-        </div>
-        <Modal 
-        isOpen={openModal} onRequestClose={() => setOpenModal(false)} shouldCloseOnOverlayClick={false}
-        style={{
-          overlay:
-          {width:700,
-             height:600,
-              backgroundColor: 'rgba(0,0,0,0.8)', 
-              zIndex:'1000',
-              top:'50%', 
-              left:"50%",
-            marginTop:'-300px',
-          marginLeft: '-350px',
-        },
-      }}
-      >
-           <div className="modal__question">
+      </div>
+      <div className="post__body">
+        <div className="post__question">
+          <p>{question}</p>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="post__btnAnswer"
+          >
+            Answer
+          </button>
+          <Modal
+            isOpen={IsmodalOpen}
+            onRequestClose={() => setIsModalOpen(false)}
+            shouldCloseOnOverlayClick={false}
+            style={{
+              overlay: {
+                width: 700,
+                height: 600,
+                backgroundColor: "rgba(0,0,0,0.8)",
+                zIndex: "1000",
+                top: "50%",
+                left: "50%",
+                marginTop: "-300px",
+                marginLeft: "-350px",
+              },
+            }}
+          >
+            <div className="modal__question">
               <h1>{question}</h1>
               <p>
-                asked by
-                <span className="name">{user.displayName ?user.user: user.email}  </span>{" "}
-                on
+                asked by{" "}
+                <span className="name">
+                  {quoraUser.displayName
+                    ? quoraUser.displayName
+                    : quoraUser.email}
+                </span>{" "}
+                {""}
+                on{" "}
                 <span className="name">
                   {new Date(timestamp?.toDate()).toLocaleString()}
                 </span>
@@ -99,15 +146,14 @@ const Post = ({id, question, imgUrl, timestamp, quoraUser}) => {
             </div>
             <div className="modal__answer">
               <textarea
-              required
                 value={answer}
-                onChange = {(e) => setAnswer(e.target.value)}
+                onChange={(e) => setAnswer(e.target.value)}
                 placeholder="Enter Your Answer"
                 type="text"
               />
             </div>
             <div className="modal__button">
-              <button className="cancle" onClick={() => setOpenModal(false)}>
+              <button className="cancle" onClick={() => setIsModalOpen(false)}>
                 Cancel
               </button>
               <button type="sumbit" onClick={handleAnswer} className="add">
@@ -115,52 +161,52 @@ const Post = ({id, question, imgUrl, timestamp, quoraUser}) => {
               </button>
             </div>
           </Modal>
-
-        <div className="post_answer">
-             {getAnswers.map(({id, answers}) => (
-                <p key={id} style={{position:'relative', paddingBottom:'5px'}}>
-                  {id === answers.questionId ? (
-                    <span>
-                      {answers.answer}
-                      <br />
-                      <span style={{position:'absolute', 
-                        color:'gray',
-                        fontSize:'small',
-                        display:'flex',
-                        right:'0px',
-                        }}
-                        >
-                          <span style={{color: '#b92b27'}}>
-                            {answers.user.displayName
-                            ? answers.user.displayName
-                          :answers.user.email}{" "}
-                          on{" "}
-                          {new Date(answers.timestamp?.toDate()).toLocaleString()}
-                          </span>
-                        </span>
+        </div>
+        <div className="post__answer">
+          {getAnswers.map(({ id, answers }) => (
+            <p key={id} style={{ position: "relative", paddingBottom: "5px" }}>
+              {Id === answers.questionId ? (
+                <span>
+                  {answers.answer}
+                  <br />
+                  <span
+                    style={{
+                      position: "absolute",
+                      color: "gray",
+                      fontSize: "small",
+                      display: "flex",
+                      right: "0px",
+                    }}
+                  >
+                    <span style={{ color: "#b92b27" }}>
+                      {answers.user.displayName
+                        ? answers.user.displayName
+                        : answers.user.email}{" "}
+                      on{" "}
+                      {new Date(answers.timestamp?.toDate()).toLocaleString()}
                     </span>
-                  ) : ("")}
-                </p>
-
-              ))}
-                
+                  </span>
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
+          ))}
         </div>
-        <img src={imgUrl} //image inserted for post 
-             alt='imgURL' />
-     </div>
-     <div className="post_footer">
-        <div className="post_footerAction">
-            <ArrowUpwardOutlined />
-            <ArrowDownwardOutlined />
+        <img src={image} alt="" />
+      </div>
+      <div className="post__footer">
+        <div className="post__footerAction">
+          <ArrowUpwardOutlined />
+          <ArrowDownwardOutlined />
         </div>
-
         <RepeatOutlined />
         <ChatBubbleOutlineOutlined />
-        <div className="post_footerLeft">
-            <ShareOutlined />
-            <MoreHorizOutlined />
+        <div className="post__footerLeft">
+          <ShareOutlined />
+          <MoreHorizOutlined />
         </div>
-     </div>
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,28 @@
-import firebase from "firebase/compat/app";
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAek7mp4gLDZxmq2cKBQxBK9z9xTdcNmW0",
-    authDomain: "quora-c14ec.firebaseapp.com",
-    projectId: "quora-c14ec",
-    storageBucket: "quora-c14ec.appspot.com",
-    messagingSenderId: "706356636712",
-    appId: "1:706356636712:web:664bc850ba2e39b9c0591d",
-    measurementId: "G-4YVSVYFSQ3"
-  };
+  apiKey: "AIzaSyAek7mp4gLDZxmq2cKBQxBK9z9xTdcNmW0",
+  authDomain: "quora-c14ec.firebaseapp.com",
+  databaseURL: "https://quora-c14ec-default-rtdb.firebaseio.com",
+  projectId: "quora-c14ec",
+  storageBucket: "quora-c14ec.appspot.com",
+  messagingSenderId: "706356636712",
+  appId: "1:706356636712:web:664bc850ba2e39b9c0591d",
+  measurementId: "G-4YVSVYFSQ3"
+};
 
-  const firebaseApp =firebase.initializeApp(firebaseConfig);
-  const auth =firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+  // Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-  const db = firebaseApp.firestore();
+const provider = new GoogleAuthProvider();
+const db = getFirestore(firebaseApp);
 
-  export{auth, provider};
-  export default db;
+
+export { auth, provider };
+export default db;
